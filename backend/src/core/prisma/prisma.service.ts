cf17e31cfe22353397fadeef8222d1a5f273261e
alijuvance <alijuvance@gmail.com>
2026-06-22 19:51:16 +0300

    fix: restore PrismaPg adapter to PrismaService

--- backend/src/core/prisma/prisma.service.ts
+++ backend/src/core/prisma/prisma.service.ts
@@ -1,14 +1,17 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    super({ errorFormat: 'pretty' });
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    super({ adapter, errorFormat: 'pretty' });
   }
   async onModuleInit() {
     await this.$connect();
   }
 }
-
-
